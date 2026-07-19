bc0378d762d39eedeee2c8c4b16798f346b11a6c
JordyLoor31 <e1317238366@live.uleam.edu.ec>
2026-07-19 13:30:26 -0500

    New: now run apphost

--- ControlTransito.AppHost/AppHost.cs
+++ ControlTransito.AppHost/AppHost.cs
@@ -1,3 +1,25 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var serviceBus = builder
+    .AddAzureServiceBus("servicebus")
+    .RunAsEmulator();
+
+var postgres = builder
+    .AddPostgres("postgres")
+    .WithLifetime(ContainerLifetime.Persistent);
+
+var db = postgres.AddDatabase("transitodb");
+
+builder.AddProject<Projects.ApiIngesta>("apiingesta")
+    .WithReference(serviceBus)
+    .WithReference(db);
+
+builder.AddProject<Projects.ApiMultas>("apimultas")
+    .WithReference(serviceBus)
+    .WithReference(db);
+
+builder.AddProject<Projects.ClienteCamara>("clientecamara");
+
+builder.AddProject<Projects.PortalCiudadano>("portalciudadano");
+
 builder.Build().Run();
